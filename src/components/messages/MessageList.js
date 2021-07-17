2207c5fe8f99fdf3a172642ceacb44bca371edc0
Luz Angelique Madrazo <luzm321@yahoo.com>
2021-07-17 00:24:56 -0500

    created functionality to add a new message that will render on the messages list

--- src/components/messages/MessageList.js
+++ src/components/messages/MessageList.js
@@ -1,6 +1,6 @@
 // Luz Angelique Madrazo || Component responsible for rendering the list of messages
 
-import React, { useEffect, useContext } from "react";
+import React, { useEffect, useContext, useState } from "react";
 import { MessageContext } from "./MessageProvider";
 import { MessageCard } from "./MessageCard";
 import "./Message.css";
@@ -8,13 +8,31 @@ import "./Message.css";
 
 export const MessageList = () => {
 
-    const { messages, getMessages } = useContext(MessageContext);
+    const { messages, getMessages, addMessage } = useContext(MessageContext);
 
     useEffect(()=>{
         console.log("MessageList: useEffect - getMessages, Initial render before data");
         getMessages()
     }, []);
 
+
+    const [message, setMessage] = useState({
+        userId: 0,
+        body: "",
+        isPrivate: false
+    });
+
+    const handleControlledInputChange = (event) => {
+        /* When changing a state object or array,
+        always create a copy, make changes, and then set state.*/
+        const newMessage = { ...message }
+        /* Message is an object with properties.
+        Set the property to the new value
+        using object bracket notation. */
+        newMessage[event.target.id] = event.target.value
+        // update state
+        setMessage(newMessage)
+    };
 
     return (
         <>
@@ -26,6 +44,20 @@ export const MessageList = () => {
                         return <MessageCard key={message.id} message={message} />;
                     })
                 }
+            </div>
+            <div class="field">
+                <label class="label" for="messageContent">New Message:</label>
+                <textarea id="body" type="text" name="messageContent" class="content" rows="1" cols="60"placeholder="Type Message..." onChange={(event) => {handleControlledInputChange(event)}}>
+                </textarea>
+                <img className="sendMsgBut" src="https://img.icons8.com/color/60/000000/filled-sent.png" onClick={() => {
+                    let userFound = messages.find((message) => {
+                        if (message.user.id === parseInt(sessionStorage.getItem("nutshell_user"))) {
+                            return message.user
+                        }
+                   });
+                   message.userId = userFound.id;
+                   addMessage(message);
+                }}/>
             </div>
         </>
       );
--- src/components/messages/MessageProvider.js
+++ src/components/messages/MessageProvider.js
@@ -16,10 +16,44 @@ export const MessageProvider = (props) => {
         .then(setMessages)
     };
 
+    let addMessage = (messageObj) => {
+        return fetch("http://localhost:8088/messages", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(messageObj)
+        })
+        .then(getMessages)
+    };
+
+    const getMessageById = (id) => {
+        return fetch(`http://localhost:8088/messages/${id}?_expand=user`)
+            .then(res => res.json())
+    };
+
+    const deleteMessage = (messageId) => {
+        return fetch(`http://localhost:8088/messages/${messageId}`, {
+            method: "DELETE"
+        })
+            .then(getMessages)
+    };
+
+    const updateMessage = (message) => {
+        return fetch(`http://localhost:8088/messages/${message.id}`, {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify(message)
+        })
+          .then(getMessages)
+      };
+
 
     return (
         <MessageContext.Provider value={{
-            messages, getMessages
+            messages, getMessages, addMessage, deleteMessage, getMessageById, updateMessage
         }}>
             {props.children}
         </MessageContext.Provider>
