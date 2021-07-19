bea85b0e03a0505c02c9e25798457802ac3135a5
mtrevin93 <mtrevin93@gmail.com>
2021-07-19 17:22:43 -0400

    added delete friend option

--- src/components/friends/FriendCard.js
+++ src/components/friends/FriendCard.js
@@ -1,15 +1,15 @@
 //Built by Michael Trevino. This module creates individual friend elements.
-import React, {useContext, useEffect} from "react"
+import React, { useContext } from "react"
 import { FriendContext } from "./FriendProvider"
 import "./Friends.css"
 //Get user data to match name to passed friend ID
 
 export const FriendCard = ({ friend }) => {
-    
+
     const { deleteFriend } = useContext(FriendContext)
     
-    const handleDelete = (friendId) => {
-        deleteFriend(friendId)
+    const handleDelete = () => {
+        deleteFriend(friend.id)
     }
 
     return (
@@ -18,9 +18,8 @@ export const FriendCard = ({ friend }) => {
         <h3 className="friend__name">
             You are friends with {friend.name}
         </h3>
-        <button onClick={handleDelete(friend.id)}>Remove Friend</button>
+        <button onClick={() => {handleDelete(friend.id)}}>Remove Friend</button>
         </section>
         </>
     )
 }
-
