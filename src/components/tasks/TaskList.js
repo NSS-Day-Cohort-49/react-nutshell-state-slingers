7f9dbc4f0738645b9280368b94b46db6d2f1f806
KippMinton <luciankippminton@gmail.com>
2021-07-21 16:01:01 -0500

    completed tasks display in separate list

--- src/components/tasks/TaskList.js
+++ src/components/tasks/TaskList.js
@@ -22,18 +22,28 @@ export const TaskList = () => {
       <h1 className="task-header">Tasks</h1>
       <div className="tasks">
 
-      <button className="add-task" onClick={() => history.push("/tasks/create")}>
-        Add New Task
-      </button>
-        {
-          tasks.map(task => {
-            let taskUser = task.userId
-            let sessionUser = parseInt(sessionStorage.getItem("nutshell_user"))
-            if(taskUser === sessionUser && task.isCompleted === false){
-            return <TaskCard key={task.id} task={task} />
+        <button className="add-task" onClick={() => history.push("/tasks/create")}>
+          Add New Task
+        </button>
+          {
+            tasks.map(task => {
+              let taskUser = task.userId
+              let sessionUser = parseInt(sessionStorage.getItem("nutshell_user"))
+              if(taskUser === sessionUser && task.isCompleted === false){
+                return <TaskCard key={task.id} task={task} />
+              }
+            })
+          }
+            <h1 className="task-header">Completed Tasks</h1>
+            {
+              tasks.map(task => {
+              let taskUser = task.userId
+              let sessionUser = parseInt(sessionStorage.getItem("nutshell_user"))
+              if (taskUser === sessionUser && task.isCompleted === true) {
+                return <TaskCard key={task.id} task={task} />
+              }
+            })
             }
-          })
-        }
       </div>
     </>
   )
